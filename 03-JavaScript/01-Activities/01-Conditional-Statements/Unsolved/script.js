ce22b7cbec963132492ac27e5c1285174f4851a8
santiago.hurtado314@gmail.com <santiago.hurtado314@gmail.com>
2022-01-04 21:07:30 -0500

    if else for loop

--- 03-JavaScript/01-Activities/01-Conditional-Statements/Unsolved/script.js
+++ 03-JavaScript/01-Activities/01-Conditional-Statements/Unsolved/script.js
@@ -4,3 +4,37 @@ var expression1 = x < 25;
 var expression2 = x > 50;
 
 // TODO: Write Your JavaScript Code Here
+
+
+if(expression1 && expression2){
+    console.log('ture✅ true✅');
+}
+
+
+else if(expression1){
+    console.log('true✅ false❌');
+}
+
+
+else if(expression2){
+    console.log('false❌ true✅');
+}
+
+
+else{
+    console.log('false❌ false❌');
+}
+
+switch (expression1 && expression2)
+{
+    case (x > 25 && x < 50):
+        console.log('ture');
+        break;
+    case (x < 25 && x > 50):
+        console.log('false');
+        break;
+}
+
+
+
+ 
--- 03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
@@ -1,1 +1,21 @@
 // TODO: Write Your JavaScript Code Here
+
+var students = ['Sarah', 'Orlando', 'Ismael', 'Hung'];
+
+//lenght of the array 
+console.log(students.length)
+
+//for loop starts at 0 runs while i is less than lenght of student arry
+// i++ = increment by 1
+for(var i = 0; i < students.length; i++)
+{
+    console.log('Great to see you, ' + students[i] + '!');
+
+}
+
+var i = 0;
+while ( i < students.length)
+{
+    console.log('Great to see you, ' + students[i] + '!');
+    i++;
+}
